43a8a781e71dae516276d567238164ed55d727a7
HULK <2530758387@qq.com>
2025-03-22 17:25:07 +0800

    认证和Redis的搭建

---
+++ homemaking-auth/src/main/java/com/successTeam/auth/AuthApplication.java
@@ -0,0 +1,11 @@
+package com.successTeam.auth;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(AuthApplication.class, args);
+    }
+}
---
+++ homemaking-common/homemaking-common-redis/src/main/java/com/successTeam/redis/config/RedisConfig.java
@@ -0,0 +1,41 @@
+package com.successTeam.redis.config;
+
+import com.successTeam.redis.config.FastJson2JsonRedisSerializer;
+import com.successTeam.redis.service.RedisService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisService redisService(RedisTemplate redisTemplate)
+    {
+        RedisService redisService = new RedisService();
+        redisService.setRedisTemplate(redisTemplate);
+        return redisService;
+    }
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory)
+    {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        FastJson2JsonRedisSerializer serializer = new FastJson2JsonRedisSerializer(Object.class);
+
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+
+        // Hash的key也采用StringRedisSerializer的序列化方式
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
