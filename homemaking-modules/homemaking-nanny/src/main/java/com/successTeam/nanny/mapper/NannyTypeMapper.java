c7874a3d61e56bd97b1c1b54d4252d4a32659b3b
Qunnn <2082637970@qq.com>
2025-03-25 14:39:28 +0800

    月嫂分类

---
+++ homemaking-modules/homemaking-nanny/src/main/java/com/successTeam/nanny/mapper/NannyTypeMapper.java
@@ -0,0 +1,20 @@
+package com.successTeam.nanny.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.successTeam.nanny.pojo.entity.NannyType;
+import org.bouncycastle.crypto.engines.NaccacheSternEngine;
+
+import java.util.List;
+
+/**
+ * com.successTeam.nanny.mapper
+ * User: hdh
+ * Date: 2025/03/24 18:51
+ * motto:   百折不挠
+ * Description:
+ * Version: V1.0
+ */
+public interface NannyTypeMapper extends BaseMapper<NannyType> {
+//    List<NannyType>  findNannyType();
+
+}
