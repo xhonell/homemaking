35cf95d95d7b48d8a69e127f86d2a8b558669442
Qunnn <2082637970@qq.com>
2025-03-27 19:47:48 +0800

    修改月嫂服务

--- homemaking-modules/homemaking-employee/src/main/java/com/successTeam/employee/controller/admin/AuditAdminController.java
+++ homemaking-modules/homemaking-employee/src/main/java/com/successTeam/employee/controller/admin/AuditAdminController.java
@@ -1,4 +1,4 @@
-package com.successTeam.employee.controller.app;
+package com.successTeam.employee.controller.admin;
 
 import com.successTeam.core.result.Result;
 import com.successTeam.employee.pojo.dto.AuditDto;
@@ -6,32 +6,33 @@ import com.successTeam.employee.service.AuditService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * com.successTeam.employee.controller.app
+ * com.successTeam.employee.controller.admin
  * User: hdh
- * Date: 2025/03/27 16:48
+ * Date: 2025/03/27 19:25
  * motto:   百折不挠
  * Description:
  * Version: V1.0
  */
-
 @RestController
-@RequestMapping("/app-api/audit")
+@RequestMapping("/admin-api/audit")
 @Api(tags = "员工档案审核接口")
-public class AuditController {
+public class AuditAdminController {
+    @Autowired
+    private AuditService auditService;
 
-//    @Autowired
-//    private AuditService auditService;
-//
-//    @PostMapping("/updateAudit")
-//    @ApiOperation("员工档案审核")
-//    public Result updateAudit(AuditDto auditDto) {
-//        auditService.updateAudit(auditDto);
-//        return Result.buildSuccess();
-//    }
+    @PostMapping("/updateAudit")
+    @ApiOperation("员工档案审核")
+    public Result AuditAdminController(AuditDto auditDto) {
+
+        auditService.updateAudit(auditDto);
+        return Result.buildSuccess();
+
+    }
+
+
 }
